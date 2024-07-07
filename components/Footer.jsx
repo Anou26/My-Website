a0efe9c06de85701a0c05ce3e94a10d2f0addd59
Anou26 <amergoju@syr.edu>
2024-07-06 20:23:33 -0400

    Removed Hire Me button from Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,12 @@
 "use client";
 import Link from "next/link";
-import { Button } from "./ui/button";
 import { useRouter } from 'next/navigation';
 
 const Footer = () => {
-    const router = useRouter();
-
-    const handleClick = () => {
-        window.open('https://www.linkedin.com/in/anoushkamergoju/', '_blank');
-        router.push('/Contact');
-    };
-
     return (
         <footer className="py-4 bg-primary text-white">
             <div className="container mx-auto flex justify-between items-center">
                 <p>&copy; {new Date().getFullYear()} Anoushka Mergoju. All rights reserved.</p>
-                
             </div>
         </footer>
     );
